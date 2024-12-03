2b83902995408eb2e5f3eb534ff474c199cd8fef
tWerwolf <115573565+tWerwolf@users.noreply.github.com>
2024-12-03 23:01:44 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,16 +1,8 @@
 <?php
     $servername = 'blogoinatorserver.mysql.database.azure.com';
-    $username = 'blogoinatoradmin@blogoinatorserver.mysql.database.azure.com';
+    $username = 'blogoinatoradmin@blogoinatorserver';
     $password = 'zaq1@WSX';
     $dbname = 'blog';
 
-    $mysqli = new mysqli($servername, $username, $password, $dbname, 3306);
-
-    // Użyj SSL
-    $mysqli->ssl_set(NULL, NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL); // Upewnij się, że ścieżka do certyfikatu jest poprawna
-    if (!$mysqli->real_connect($servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL)) {
-        die("Connection failed: " . $mysqli->connect_error);
-    }
-
-    echo "Connected successfully with SSL!";
+    $conn = new mysqli($servername, $username, $password, $dbname);
 ?>
