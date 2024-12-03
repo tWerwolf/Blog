21c8381dd0c905e986f060bd30faca3f6d6e39de
tWerwolf <115573565+tWerwolf@users.noreply.github.com>
2024-12-03 22:33:22 +0100

    Update config.php

--- config.php
+++ config.php
@@ -9,4 +9,11 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     } 
+
+    $mysqli->ssl_set(NULL, NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);  // Wskaż ścieżkę do certyfikatu
+    if (!$mysqli->real_connect($servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL)) {
+        die("Connection failed: " . $mysqli->connect_error);
+    }
+
+    echo "Connected successfully with SSL!";
 ?>
