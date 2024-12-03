fbe4be0384318a0b4dbc81c62cf9e10d3f051785
tWerwolf <115573565+tWerwolf@users.noreply.github.com>
2024-12-03 23:07:15 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_init();
-    mysqli_ssl_set($con,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+    $conn = mysqli_init();
+    mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
     mysqli_real_connect($conn, "blogoinatorserver.mysql.database.azure.com", "blogoinatoradmin", "zaq1@WSX", "blog", 3306, MYSQLI_CLIENT_SSL);
 ?>
